e529493a7f0affa0b01c5bf054ddb07b00b8d352
Artur Dryomov <me@ming13.ru>
2012-04-29 02:52:32 +0300

    Add departure management to Route class.

--- src/main/java/app/android/bustime/local/Route.java
+++ src/main/java/app/android/bustime/local/Route.java
@@ -1,7 +1,11 @@
 package app.android.bustime.local;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 
@@ -72,5 +76,74 @@ public class Route
 
 		database.update(DbTableNames.ROUTES, databaseValues,
 			String.format("%s = %d", DbFieldNames.ID, id), null);
+	}
+
+	public void insertDepartureTime(Time time) {
+		database.beginTransaction();
+
+		try {
+			tryInsertDepartureTime(time);
+			database.setTransactionSuccessful();
+		}
+		finally {
+			database.endTransaction();
+		}
+	}
+
+	private void tryInsertDepartureTime(Time time) {
+		ContentValues databaseValues = new ContentValues();
+
+		databaseValues.put(DbFieldNames.ROUTE_ID, id);
+		databaseValues.put(DbFieldNames.DEPARTURE_TIME, time.toString());
+
+		database.insert(DbTableNames.TRIPS, null, databaseValues);
+	}
+
+	public void removeDepartureTime(Time time) {
+		database.beginTransaction();
+
+		try {
+			tryRemoveDepartureTime(time);
+			database.setTransactionSuccessful();
+		}
+		finally {
+			database.endTransaction();
+		}
+	}
+
+	private void tryRemoveDepartureTime(Time time) {
+		database.delete(DbTableNames.TRIPS,
+			String.format("%s = %s", DbFieldNames.DEPARTURE_TIME, time.toString()), null);
+	}
+
+	public List<Time> getDepartureTimetable() {
+		List<Time> departureTimetable = new ArrayList<Time>();
+
+		Cursor databaseCursor = database.rawQuery(buildDepertureTimetableSelectionQuery(), null);
+
+		while (databaseCursor.moveToNext()) {
+			String timeAsString = extractTimeFromCursor(databaseCursor);
+			departureTimetable.add(new Time(timeAsString));
+		}
+
+		return departureTimetable;
+	}
+
+	private String buildDepertureTimetableSelectionQuery() {
+		StringBuilder queryBuilder = new StringBuilder();
+
+		queryBuilder.append("select ");
+
+		queryBuilder.append(String.format("%s ", DbFieldNames.DEPARTURE_TIME));
+
+		queryBuilder.append(String.format("from %s ", DbTableNames.TRIPS));
+		queryBuilder.append(String.format("where %s = %d", DbFieldNames.ROUTE_ID, id));
+
+		return queryBuilder.toString();
+	}
+
+	private String extractTimeFromCursor(Cursor databaseCursor) {
+		return databaseCursor.getString(databaseCursor
+			.getColumnIndexOrThrow(DbFieldNames.DEPARTURE_TIME));
 	}
 }
