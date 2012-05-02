54038e9b54c62aa4a682162ac94b0a54bd857f82
Artur Dryomov <me@ming13.ru>
2012-05-03 00:33:13 +0300

    Add ability to choose existing station for route shift time.

--- application/src/main/java/app/android/bustime/ui/StationCreationActivity.java
+++ application/src/main/java/app/android/bustime/ui/StationCreationActivity.java
@@ -4,6 +4,7 @@ package app.android.bustime.ui;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import android.app.Activity;
 import android.content.Context;
@@ -13,7 +14,11 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
+import android.widget.SimpleAdapter;
+import android.widget.Spinner;
 import android.widget.TimePicker;
 import app.android.bustime.R;
 import app.android.bustime.local.DbException;
@@ -29,68 +34,45 @@ public class StationCreationActivity extends Activity
 
 	private Route route;
 	private List<Station> stations;
+
+	private Station chosenExistingStation;
 	private String stationName;
 	private int shiftTimeHour;
 	private int shiftTimeMinute;
 
-	private final List<HashMap<String, Object>> stationsList = new ArrayList<HashMap<String, Object>>();
+	private boolean isStationExist;
+
+	private final List<HashMap<String, Object>> stationsData;
 	private static final String SPINNER_ITEM_TEXT_ID = "text";
 	private static final String SPINNER_ITEM_OBJECT_ID = "object";
+
+	public StationCreationActivity() {
+		super();
+
+		stationsData = new ArrayList<HashMap<String, Object>>();
+		isStationExist = false;
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.station_creation);
 
-		initializeBodyControls();
-
 		processReceivedRoute();
-		setUpNullTime();
 
-		loadStations();
-		// initializeStationsSpinner();
+		initializeBodyControls();
 	}
 
-	private void fillStationsList() {
+	private void processReceivedRoute() {
+		Bundle receivedData = this.getIntent().getExtras();
 
-	}
-
-
-	private void initializeStationsSpinner() {
-		if (stations.isEmpty()) {
-			CheckBox stationWasCreateBox = (CheckBox) findViewById(R.id.stationWasCreatedCheckbox);
-			stationWasCreateBox.setVisibility(View.GONE);
-
-			return;
+		if (receivedData.containsKey(IntentFactory.MESSAGE_ID)) {
+			route = receivedData.getParcelable(IntentFactory.MESSAGE_ID);
 		}
+		else {
+			UserAlerter.alert(activityContext, getString(R.string.someError));
 
-	}
-
-	private void loadStations() {
-		new LoadStationsTask().execute();
-	}
-
-	private class LoadStationsTask extends AsyncTask<Void, Void, String>
-	{
-		@Override
-		protected String doInBackground(Void... params) {
-			try {
-				stations = DbProvider.getInstance().getStations().getStationsList();
-			}
-			catch (DbException e) {
-				return getString(R.string.noStations);
-			}
-
-			return new String();
-		}
-
-		@Override
-		protected void onPostExecute(String errorMessage) {
-			super.onPostExecute(errorMessage);
-
-			if (!errorMessage.isEmpty()) {
-				UserAlerter.alert(activityContext, errorMessage);
-			}
+			finish();
 		}
 	}
 
@@ -100,6 +82,12 @@ public class StationCreationActivity extends Activity
 
 		TimePicker shiftTimePicker = (TimePicker) findViewById(R.id.shiftTimePicker);
 		shiftTimePicker.setIs24HourView(true);
+		setUpNullTimeToShiftTimePicker();
+
+		CheckBox stationWasCreatedCheckbox = (CheckBox) findViewById(R.id.stationWasCreatedCheckbox);
+		stationWasCreatedCheckbox.setOnCheckedChangeListener(isStationExistListener);
+
+		fillStationsSpinner();
 	}
 
 	private final OnClickListener confirmListener = new OnClickListener() {
@@ -123,16 +111,36 @@ public class StationCreationActivity extends Activity
 	};
 
 	private void readUserDataFromFields() {
-		EditText stationNameEdit = (EditText) findViewById(R.id.stationNameEdit);
 		TimePicker shiftTimePicker = (TimePicker) findViewById(R.id.shiftTimePicker);
-
-		stationName = stationNameEdit.getText().toString().trim();
 		shiftTimeHour = shiftTimePicker.getCurrentHour();
 		shiftTimeMinute = shiftTimePicker.getCurrentMinute();
+
+		if (isStationExist) {
+			chosenExistingStation = getChoosedStation();
+		}
+		else {
+			EditText stationNameEdit = (EditText) findViewById(R.id.stationNameEdit);
+			stationName = stationNameEdit.getText().toString().trim();
+		}
+	}
+
+	private Station getChoosedStation() {
+		Spinner stationsSpinner = (Spinner) findViewById(R.id.stationsSpinner);
+
+		@SuppressWarnings("unchecked")
+		Map<String, Object> stationSpinnerItem = (Map<String, Object>) stationsSpinner
+			.getSelectedItem();
+
+		return (Station) stationSpinnerItem.get(SPINNER_ITEM_OBJECT_ID);
 	}
 
 	private String getUserDataErrorMessage() {
-		return getStationNameErrorMessage();
+		if (isStationExist) {
+			return new String();
+		}
+		else {
+			return getStationNameErrorMessage();
+		}
 	}
 
 	private String getStationNameErrorMessage() {
@@ -148,8 +156,18 @@ public class StationCreationActivity extends Activity
 		@Override
 		protected String doInBackground(Void... params) {
 			try {
-				Station station = DbProvider.getInstance().getStations().createStation(stationName);
-				station.insertShiftTimeForRoute(route, new Time(shiftTimeHour, shiftTimeMinute));
+				Station stationToInsertShiftTime;
+
+				if (isStationExist) {
+					stationToInsertShiftTime = chosenExistingStation;
+				}
+				else {
+					stationToInsertShiftTime = DbProvider.getInstance().getStations()
+						.createStation(stationName);
+				}
+
+				stationToInsertShiftTime.insertShiftTimeForRoute(route, new Time(shiftTimeHour,
+					shiftTimeMinute));
 			}
 			catch (DbException e) {
 				return getString(R.string.someError);
@@ -171,22 +189,102 @@ public class StationCreationActivity extends Activity
 		}
 	}
 
-	private void processReceivedRoute() {
-		Bundle receivedData = this.getIntent().getExtras();
-
-		if (receivedData.containsKey(IntentFactory.MESSAGE_ID)) {
-			route = receivedData.getParcelable(IntentFactory.MESSAGE_ID);
-		}
-		else {
-			UserAlerter.alert(activityContext, getString(R.string.someError));
-
-			finish();
-		}
-	}
-
-	private void setUpNullTime() {
+	private void setUpNullTimeToShiftTimePicker() {
 		TimePicker shiftTimePicker = (TimePicker) findViewById(R.id.shiftTimePicker);
 		shiftTimePicker.setCurrentHour(0);
 		shiftTimePicker.setCurrentMinute(0);
+	}
+
+	private final OnCheckedChangeListener isStationExistListener = new OnCheckedChangeListener() {
+		@Override
+		public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+			isStationExist = isChecked;
+
+			updateBodyControls();
+		}
+	};
+
+	private void updateBodyControls() {
+		EditText stationNameEdit = (EditText) findViewById(R.id.stationNameEdit);
+		Spinner stationsListSpinner = (Spinner) findViewById(R.id.stationsSpinner);
+
+		if (isStationExist) {
+			stationNameEdit.setVisibility(View.GONE);
+			stationsListSpinner.setVisibility(View.VISIBLE);
+		}
+		else {
+			stationNameEdit.setVisibility(View.VISIBLE);
+			stationsListSpinner.setVisibility(View.GONE);
+		}
+	}
+
+	private void fillStationsSpinner() {
+		new LoadStationsTask().execute();
+	}
+
+	private class LoadStationsTask extends AsyncTask<Void, Void, String>
+	{
+		@Override
+		protected String doInBackground(Void... params) {
+			try {
+				stations = DbProvider.getInstance().getStations().getStationsList();
+			}
+			catch (DbException e) {
+				return getString(R.string.noStations);
+			}
+
+			return new String();
+		}
+
+		@Override
+		protected void onPostExecute(String errorMessage) {
+			super.onPostExecute(errorMessage);
+
+			if (!errorMessage.isEmpty()) {
+				UserAlerter.alert(activityContext, errorMessage);
+				return;
+			}
+
+			if (stations.isEmpty()) {
+				hidePossibilityToChooseExistingStation();
+				return;
+			}
+
+			setUpStationsSpinner();
+		}
+	}
+
+	private void hidePossibilityToChooseExistingStation() {
+		CheckBox stationWasCreateBox = (CheckBox) findViewById(R.id.stationWasCreatedCheckbox);
+		stationWasCreateBox.setVisibility(View.GONE);
+	}
+
+	private void setUpStationsSpinner() {
+		fillStationsData();
+
+		SimpleAdapter stationsAdapter = new SimpleAdapter(activityContext, stationsData,
+			android.R.layout.simple_spinner_item, new String[] { SPINNER_ITEM_TEXT_ID },
+			new int[] { android.R.id.text1 });
+		stationsAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+		Spinner stationsSpinner = (Spinner) findViewById(R.id.stationsSpinner);
+		stationsSpinner.setAdapter(stationsAdapter);
+	}
+
+	private void fillStationsData() {
+		stationsData.clear();
+
+		for (Station station : stations) {
+			addItemToStationsData(station);
+		}
+	}
+
+	private void addItemToStationsData(Station station) {
+		HashMap<String, Object> stationItem = new HashMap<String, Object>();
+
+		stationItem.put(SPINNER_ITEM_TEXT_ID, station.getName());
+		stationItem.put(SPINNER_ITEM_OBJECT_ID, station);
+
+		stationsData.add(stationItem);
 	}
 }
