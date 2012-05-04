ef0dc4208fccc71f535da9a409a4cffbbb1fe93e
Artur Dryomov <me@ming13.ru>
2012-05-04 23:12:40 +0300

    Add TimeFormatter class to print Time for humans.

---
+++ application/src/main/java/app/android/bustime/ui/HumanTimeFormatter.java
@@ -0,0 +1,60 @@
+package app.android.bustime.ui;
+
+
+import android.content.Context;
+import app.android.bustime.R;
+import app.android.bustime.local.Time;
+
+
+public class HumanTimeFormatter
+{
+	private final static int MINUTE_SINGULAR_FORM_CASE = 1;
+	private final static int HOUR_SINGULAR_FORM_CASE = 1;
+
+	private final Context context;
+
+	public HumanTimeFormatter(Context context) {
+		this.context = context;
+	}
+
+	public String toHumanFormat(Time time) {
+		StringBuilder humanTimeBuilder = new StringBuilder();
+
+		final int hours = time.getHours();
+		final int minutes = time.getMinutes();
+
+		if (hours != 0) {
+			humanTimeBuilder.append(String.format("%d ", hours));
+			humanTimeBuilder.append(getHourPostfix(hours));
+		}
+
+		if ((hours != 0) && (minutes != 0)) {
+			humanTimeBuilder.append(" ");
+		}
+
+		if (minutes != 0) {
+			humanTimeBuilder.append(String.format("%d ", minutes));
+			humanTimeBuilder.append(getMinutePostfix(minutes));
+		}
+
+		return humanTimeBuilder.toString();
+	}
+
+	private String getMinutePostfix(int minutes) {
+		if (minutes == MINUTE_SINGULAR_FORM_CASE) {
+			return context.getString(R.string.minuteSingularForm);
+		}
+		else {
+			return context.getString(R.string.minutePluralForm);
+		}
+	}
+
+	private String getHourPostfix(int hours) {
+		if (hours == HOUR_SINGULAR_FORM_CASE) {
+			return context.getString(R.string.hourSingularForm);
+		}
+		else {
+			return context.getString(R.string.hourPluralForm);
+		}
+	}
+}
