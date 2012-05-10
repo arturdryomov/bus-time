2503fcea9a235eeccccee5c2d0bb573b3a2362aa
Artur Dryomov <me@ming13.ru>
2012-05-11 00:27:12 +0300

    Change database name for better filesystem navigation.

--- application/src/main/java/app/android/bustime/local/DbOpenHelper.java
+++ application/src/main/java/app/android/bustime/local/DbOpenHelper.java
@@ -9,7 +9,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 class DbOpenHelper extends SQLiteOpenHelper
 {
 	private static final int DATABASE_VERSION = 2;
-	private static final String DATABASE_NAME = "BusTime";
+	private static final String DATABASE_NAME = "bustime.db";
 
 	public DbOpenHelper(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
