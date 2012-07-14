0d34c123d6ec6f6c5ad73ce80417fb73658069b9
Artur Dryomov <me@ming13.ru>
2012-07-14 12:46:47 +0300

    Add dummy database copying from assets.

--- application/src/main/java/app/android/bustime/db/DbOpenHelper.java
+++ application/src/main/java/app/android/bustime/db/DbOpenHelper.java
@@ -1,9 +1,18 @@
 package app.android.bustime.db;
 
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import org.apache.commons.io.IOUtils;
 
 
 class DbOpenHelper extends SQLiteOpenHelper
@@ -11,118 +20,58 @@ class DbOpenHelper extends SQLiteOpenHelper
 	private static final int DATABASE_VERSION = 2;
 	private static final String DATABASE_NAME = "bustime.db";
 
+	private final Context context;
+
 	public DbOpenHelper(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+
+		this.context = context;
 	}
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		db.beginTransaction();
+		removeFile(getDatabaseFile(db));
+		copyData(getSourceDatabaseStream(), getDatabaseStream(db));
+	}
+
+	private void removeFile(File file) {
+		file.delete();
+	}
+
+	private InputStream getSourceDatabaseStream() {
+		AssetManager assetManager = context.getAssets();
 
 		try {
-			createTables(db);
-
-			db.setTransactionSuccessful();
+			return assetManager.open(DATABASE_NAME);
 		}
-		finally {
-			db.endTransaction();
+		catch (IOException e) {
+			throw new DbException();
 		}
 	}
 
-	private void createTables(SQLiteDatabase db) {
-		db.execSQL(buildRoutesTableCreationQuery());
-		db.execSQL(buildTripsTableCreationQuery());
-		db.execSQL(buildStationsTableCreationQuery());
-		db.execSQL(buildRoutesAndStationsTableCreationQuery());
+	private OutputStream getDatabaseStream(SQLiteDatabase db) {
+		try {
+			return new FileOutputStream(getDatabaseFile(db));
+		}
+		catch (FileNotFoundException e) {
+			throw new DbException();
+		}
 	}
 
-	private String buildRoutesTableCreationQuery() {
-		StringBuilder queryBuilder = new StringBuilder();
-
-		queryBuilder.append(String.format("create table if not exists %s ", DbTableNames.ROUTES));
-
-		queryBuilder.append("(");
-		queryBuilder.append(String.format("%s %s, ", DbFieldNames.ID, DbFieldParameters.ID));
-		queryBuilder.append(String.format("%s %s", DbFieldNames.NAME, DbFieldParameters.NAME));
-		queryBuilder.append(")");
-
-		return queryBuilder.toString();
+	private File getDatabaseFile(SQLiteDatabase db) {
+		return new File(db.getPath());
 	}
 
-	private String buildTripsTableCreationQuery() {
-		StringBuilder queryBuilder = new StringBuilder();
-
-		queryBuilder.append(String.format("create table if not exists %s ", DbTableNames.TRIPS));
-
-		queryBuilder.append("(");
-		queryBuilder.append(String.format("%s %s, ", DbFieldNames.ID, DbFieldParameters.ID));
-		queryBuilder.append(
-			String.format("%s %s, ", DbFieldNames.ROUTE_ID, DbFieldParameters.FOREIGN_ROUTE_ID));
-		queryBuilder.append(
-			String.format("%s %s", DbFieldNames.DEPARTURE_TIME, DbFieldParameters.TIME));
-		queryBuilder.append(")");
-
-		return queryBuilder.toString();
-	}
-
-	private String buildStationsTableCreationQuery() {
-		StringBuilder queryBuilder = new StringBuilder();
-
-		queryBuilder.append(String.format("create table if not exists %s ", DbTableNames.STATIONS));
-
-		queryBuilder.append("(");
-		queryBuilder.append(String.format("%s %s, ", DbFieldNames.ID, DbFieldParameters.ID));
-		queryBuilder.append(String.format("%s %s, ", DbFieldNames.NAME, DbFieldParameters.NAME));
-		queryBuilder.append(
-			String.format("%s %s, ", DbFieldNames.LATITUDE, DbFieldParameters.STATION_COORDINATE));
-		queryBuilder.append(
-			String.format("%s %s", DbFieldNames.LONGITUDE, DbFieldParameters.STATION_COORDINATE));
-		queryBuilder.append(")");
-
-		return queryBuilder.toString();
-	}
-
-	private String buildRoutesAndStationsTableCreationQuery() {
-		StringBuilder queryBuilder = new StringBuilder();
-
-		queryBuilder.append(
-			String.format("create table if not exists %s ", DbTableNames.ROUTES_AND_STATIONS));
-
-		queryBuilder.append("(");
-		queryBuilder.append(String.format("%s %s, ", DbFieldNames.ID, DbFieldParameters.ID));
-		queryBuilder.append(
-			String.format("%s %s, ", DbFieldNames.ROUTE_ID, DbFieldParameters.FOREIGN_ROUTE_ID));
-		queryBuilder.append(
-			String.format("%s %s, ", DbFieldNames.STATION_ID, DbFieldParameters.FOREIGN_STATION_ID));
-		queryBuilder.append(String.format("%s %s", DbFieldNames.TIME_SHIFT, DbFieldParameters.TIME));
-		queryBuilder.append(")");
-
-		return queryBuilder.toString();
+	private void copyData(InputStream inputStream, OutputStream outputStream) {
+		try {
+			IOUtils.copy(inputStream, outputStream);
+		}
+		catch (IOException e) {
+			throw new DbException();
+		}
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldDatabaseVersion, int newDatabaseVersion) {
-		db.beginTransaction();
-
-		try {
-			dropTables(db);
-			createTables(db);
-
-			db.setTransactionSuccessful();
-		}
-		finally {
-			db.endTransaction();
-		}
-	}
-
-	private void dropTables(SQLiteDatabase db) {
-		dropTable(db, DbTableNames.ROUTES_AND_STATIONS);
-		dropTable(db, DbTableNames.TRIPS);
-		dropTable(db, DbTableNames.ROUTES);
-		dropTable(db, DbTableNames.STATIONS);
-	}
-
-	private void dropTable(SQLiteDatabase db, String tableName) {
-		db.execSQL(String.format("drop table %s", tableName));
 	}
 }
