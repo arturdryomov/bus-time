23bdb812fbedae8636a1c15aeeeba209235c0bf3
Artur Dryomov <me@ming13.ru>
2012-08-09 22:58:38 +0300

    Add comment about LoaderSafeRunner goals.

--- application/src/main/java/ru/ming13/bustime/ui/util/LoaderSafeRunner.java
+++ application/src/main/java/ru/ming13/bustime/ui/util/LoaderSafeRunner.java
@@ -3,6 +3,11 @@ package ru.ming13.bustime.ui.util;
 
 import android.os.Handler;
 
+/*
+ * AsyncTaskLoaders couldn’t call FragmentTransactions from onLoadFinished() method.
+ * Solution is move actions to the handler. Ugly but Android developers think that UI shouldn’t
+ * change on application run.
+ */
 
 public final class LoaderSafeRunner
 {
