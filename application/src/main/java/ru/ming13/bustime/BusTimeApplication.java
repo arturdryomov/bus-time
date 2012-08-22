825ce41d640715f6526c5debe8244d60995eac9d
Artur Dryomov <artur.dryomov@gmail.com>
2012-08-22 22:57:19 +0300

    Add BugSense initialization.

--- application/src/main/java/ru/ming13/bustime/BusTimeApplication.java
+++ application/src/main/java/ru/ming13/bustime/BusTimeApplication.java
@@ -2,6 +2,7 @@ package ru.ming13.bustime;
 
 
 import android.app.Application;
+import com.bugsense.trace.BugSenseHandler;
 import ru.ming13.bustime.db.DbProvider;
 
 
@@ -12,9 +13,24 @@ public class BusTimeApplication extends Application
 		super.onCreate();
 
 		setUpDatabase();
+		setUpBugsense();
 	}
 
 	private void setUpDatabase() {
 		DbProvider.getInstance(this);
+	}
+
+	private void setUpBugsense() {
+		if (isBugsenseEnabled()) {
+			BugSenseHandler.setup(this, getBugsenseProjectKey());
+		}
+	}
+
+	private boolean isBugsenseEnabled () {
+		return getResources().getBoolean(R.bool.flag_bugsense_enabled);
+	}
+
+	private String getBugsenseProjectKey() {
+		return getString(R.string.key_bugsense_project);
 	}
 }
