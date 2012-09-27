fb421d3d86e663f9e06e9ca16dd08abeded39792
Artur Dryomov <artur.dryomov@gmail.com>
2012-09-27 21:30:57 +0300

    Remove unused routes loader entries.

--- application/src/main/java/ru/ming13/bustime/ui/loader/RoutesLoader.java
+++ application/src/main/java/ru/ming13/bustime/ui/loader/RoutesLoader.java
@@ -7,32 +7,12 @@ import android.content.Context;
 import android.support.v4.content.AsyncTaskLoader;
 import ru.ming13.bustime.db.DbProvider;
 import ru.ming13.bustime.db.model.Route;
-import ru.ming13.bustime.db.model.Station;
 
 
 public class RoutesLoader extends AsyncTaskLoader<List<Route>>
 {
-	private static enum Mode
-	{
-		ALL, FOR_STATION
-	}
-
-	private final Mode mode;
-
-	private Station station;
-
 	public RoutesLoader(Context context) {
 		super(context);
-
-		mode = Mode.ALL;
-	}
-
-	public RoutesLoader(Context context, Station station) {
-		super(context);
-
-		this.station = station;
-
-		mode = Mode.FOR_STATION;
 	}
 
 	@Override
@@ -44,15 +24,6 @@ public class RoutesLoader extends AsyncTaskLoader<List<Route>>
 
 	@Override
 	public List<Route> loadInBackground() {
-		switch (mode) {
-			case ALL:
-				return DbProvider.getInstance().getRoutes().getRoutesList();
-
-			case FOR_STATION:
-				return DbProvider.getInstance().getRoutes().getRoutesList(station);
-
-			default:
-				throw new LoaderException();
-		}
+		return DbProvider.getInstance().getRoutes().getRoutesList();
 	}
 }
