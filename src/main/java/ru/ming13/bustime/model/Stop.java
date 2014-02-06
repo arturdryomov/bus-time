2683d60cd5d623cc7d368d0cf2c8ba620e36fd54
Artur Dryomov <artur.dryomov@gmail.com>
2014-02-06 03:14:56 +0300

    Remove unused fields from stop model.

--- src/main/java/ru/ming13/bustime/model/Stop.java
+++ src/main/java/ru/ming13/bustime/model/Stop.java
@@ -8,15 +8,11 @@ public class Stop implements Parcelable
 	private final long id;
 	private final String name;
 	private final String direction;
-	private final double latitude;
-	private final double longitude;
 
 	public Stop(long id, String name, String direction) {
 		this.id = id;
 		this.name = name;
 		this.direction = direction;
-		this.latitude = 0;
-		this.longitude = 0;
 	}
 
 	public long getId() {
@@ -29,14 +25,6 @@ public class Stop implements Parcelable
 
 	public String getDirection() {
 		return direction;
-	}
-
-	public double getLatitude() {
-		return latitude;
-	}
-
-	public double getLongitude() {
-		return longitude;
 	}
 
 	public static final Creator<Stop> CREATOR = new Creator<Stop>()
@@ -56,8 +44,6 @@ public class Stop implements Parcelable
 		this.id = parcel.readLong();
 		this.name = parcel.readString();
 		this.direction = parcel.readString();
-		this.latitude = parcel.readDouble();
-		this.longitude = parcel.readDouble();
 	}
 
 	@Override
@@ -65,8 +51,6 @@ public class Stop implements Parcelable
 		parcel.writeLong(id);
 		parcel.writeString(name);
 		parcel.writeString(direction);
-		parcel.writeDouble(latitude);
-		parcel.writeDouble(longitude);
 	}
 
 	@Override
