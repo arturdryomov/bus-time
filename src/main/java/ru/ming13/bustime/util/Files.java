a1fc47c5f1e448dbb347d8b0c5f972d07176c27a
Artur Dryomov <artur.dryomov@gmail.com>
2016-02-28 22:04:16 +0300

    Refactor files utility class.

--- src/main/java/ru/ming13/bustime/util/Files.java
+++ src/main/java/ru/ming13/bustime/util/Files.java
@@ -3,45 +3,49 @@ package ru.ming13.bustime.util;
 import android.support.annotation.NonNull;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
 import okio.BufferedSink;
 import okio.BufferedSource;
 import okio.Okio;
+import okio.Sink;
+import okio.Source;
 
 public final class Files
 {
-    private Files() {
-    }
+	private Files() {
+	}
 
-    public static void copy(@NonNull File sourceFile, @NonNull File destinationFile) {
-        try {
-            BufferedSource source = Okio.buffer(Okio.source(sourceFile));
-            BufferedSink destination = Okio.buffer(Okio.sink(destinationFile));
+	public static void copy(@NonNull File sourceFile, @NonNull File destinationFile) {
+		try {
+			Files.copy(Okio.source(sourceFile), Okio.sink(destinationFile));
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-            destination.writeAll(source);
-            destination.flush();
+	public static void copy(@NonNull InputStream sourceStream, @NonNull File destinationFile) {
+		try {
+			Files.copy(Okio.source(sourceStream), Okio.sink(destinationFile));
+		} catch (FileNotFoundException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
-            source.close();
-            destination.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+	private static void copy(Source source, Sink destination) {
+		try {
+			BufferedSource bufferedSource = Okio.buffer(source);
+			BufferedSink bufferedDestination = Okio.buffer(destination);
 
-    public static void copy(@NonNull InputStream sourceStream, @NonNull File destinationFile) {
-        try {
-            BufferedSource source = Okio.buffer(Okio.source(sourceStream));
-            BufferedSink destination = Okio.buffer(Okio.sink(destinationFile));
+			bufferedDestination.writeAll(bufferedSource);
+			bufferedDestination.flush();
 
-            destination.writeAll(source);
-            destination.flush();
-
-            source.close();
-            destination.close();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+			bufferedSource.close();
+			bufferedDestination.close();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
